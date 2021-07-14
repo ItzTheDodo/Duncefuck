a39f8f3db3c9c8e921b90582654d11cde57aac83
ItzTheDodo <57725407+ItzTheDodo@users.noreply.github.com>
2021-07-14 12:55:13 +0100

    Add setup.py upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Duncefuck",
+    version="v0.1",
+    description="My take on a programming language",
+    author="ItzTheDodo",
+    url="https://github.com/ItzTheDodo/Duncefuck",
+    packages=['Duncefuck']
+)
